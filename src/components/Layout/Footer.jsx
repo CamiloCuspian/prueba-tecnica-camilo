2aeb8f09036bf5c577eae1a2951174bd1bf6d2d2
Juan Camilo Cuspián <cuspianhuertas@gmail.com>
2025-04-29 11:56:35 -0500

    frde

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="py-6 mt-12 bg-gray-100">
       <div className="container px-4 mx-auto">
         <div className="text-center text-gray-500">
-          <p>&copy; {new Date().getFullYear()} ProductManager. Todos los derechos reservados.</p>
+          <p>&copy; {new Date().getFullYear()} Camilo Cuspian. Todos los derechos reservados.</p>
         </div>
       </div>
     </footer>
